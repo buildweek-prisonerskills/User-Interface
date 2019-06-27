5ddd8a2220a24ec8fc00fc9b171c9182587bc745
Dakota Hoskins <dakotaahoskinss@gmail.com>
2019-06-27 16:51:17 -0400

    added mousover/mouseleave animation

--- js/landing.js
+++ js/landing.js
@@ -1,6 +1,12 @@
 const opaqueText = document.querySelectorAll('p');
 
-opaqueText.forEach( elem => elem.addEventListener('mouseover', function() {
-    console.log('Event fired.')
-    }));
+const bgImage = document.querySelector('.wrapper');
 
+opaqueText.forEach( elem => elem.addEventListener('mouseover', function() {
+    elem.style.fontWeight = '900';
+    bgImage.opacity = 0.5;
+}));
+
+opaqueText.forEach( elem => elem.addEventListener('mouseleave', function() {
+    elem.style.fontWeight = 'normal';
+}));
