f256189b99c9ff4e330e9cb8c2e14a32c42e2b16
Dakota Hoskins <dakotaahoskinss@gmail.com>
2019-06-27 17:43:57 -0400

    final commit

--- js/landing.js
+++ js/landing.js
@@ -4,7 +4,6 @@ const bgImage = document.querySelector('.wrapper');
 
 opaqueText.forEach( elem => elem.addEventListener('mouseover', function() {
     elem.style.fontWeight = '900';
-    bgImage.opacity = 0.5;
 }));
 
 opaqueText.forEach( elem => elem.addEventListener('mouseleave', function() {
